72dbce062f1ee0f3977b31a9120be4f613069ee3
ricky074game <67124402+ricky074game@users.noreply.github.com>
2022-05-02 17:14:26 -0400

    Update ItemShop.php

--- src/BedWars/game/shop/ItemShop.php
+++ src/BedWars/game/shop/ItemShop.php
@@ -35,7 +35,7 @@ class ItemShop
         3 => ["name" =>"Blocks", "image" => ""],
         4 => ["name" => "Bows", "image" => ""],
         5 => ["name" => "Potions" ,"image" => ""],
-        6 => ["name" => "Specials", "image" => ""]
+        6 => ["name" => "Specials", "image" => ""],
         7 => ["name" => "Tools", "image" => ""]
     ];
 
