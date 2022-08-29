666878dfc315bbeb3a73795168e8769227a8695c
ItsToxic <95104544+ItsToxicGG@users.noreply.github.com>
2022-08-29 21:17:58 +0200

    array

--- src/BedWars/game/shop/ItemShop.php
+++ src/BedWars/game/shop/ItemShop.php
@@ -128,7 +128,7 @@ class ItemShop
         return self::$shopWindows[$category];
     }
 
-    public static function handleTransaction(int $id, $data, Player $p, BedWars $plugin, int $formId){
+    public static function handleTransaction(int $id, array $data, Player $p, BedWars $plugin, int $formId){
         if(is_null($data)){
             return;
         }
